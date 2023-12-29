730b3e402540f320054229bfb2aca5964da68dec
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-29 15:27:10 +0100

    edit matrix type from matrix[][] to int** matrix

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-all: src/matrix.c
-	gcc src/matrix.c -o matrix
+all: src/matrix.c src/aux.c
+	gcc src/matrix.c src/aux.c -o matrix
 
 clean:
 	rm -f src/a.out matrix
--- src/matrix.c
+++ src/matrix.c
@@ -1,6 +1,6 @@
 #include "../include/matrix.h"
 
-#define PADDING 4
+#define PADDING 7
 
 
 int main(int argc, char* argv[]) 
@@ -14,12 +14,21 @@ int main(int argc, char* argv[])
     int matrix1[3][3] = {{10, 1, 123}, {-5, 3, 11}, {7, -15, 9}};
     int matrix2[3][3] = {{-14, 3, -2}, {6, 7, 8}, {-4, 1, 15}};
 
+    srand(time(0));
 
-    print_matrix(3, 3, matrix2);
-    printf("\n*\n");
-    print_matrix(3, 3, matrix1);
-    printf("\n=\n");
-    multiplication_squared(3, 3, matrix1, matrix2);
+    size_t sz = 10;
+    int** a = generate_matrix(sz, sz);
+    int** b = generate_matrix(sz, sz);
+
+    print_matrix(sz, sz, a);
+    printf("\n  *\n");
+    print_matrix(sz, sz, b);
+    printf("\n  =\n");
+
+    multiplication_squared(sz, sz, a, b);
+
+    free_matrix(sz, sz, a);
+    free_matrix(sz, sz, b);
 
     return 0;
 }
@@ -27,31 +36,23 @@ int main(int argc, char* argv[])
 /* -------------------------------------------------------------------- */
 
 
-void print_matrix(size_t rows, size_t cols, int matrix[rows][cols])
+void print_matrix(size_t rows, size_t cols, int** matrix)
 {
-    size_t size_matrix = sizeof(char) * rows * cols * (PADDING +1);
-    size_t offset = 0;
-
-    char* result = malloc(size_matrix);
-    // TODO: error handling for malloc
 
     for (int i=0; i<rows; i++) 
     {
         for (int j=0; j<cols; j++) 
         {
-            offset += sprintf(result+offset, "%*d ", PADDING,  matrix[i][j]);
+            printf("%*d ", PADDING,  matrix[i][j]);
         }
-        offset += sprintf(result+offset, "\n");
+        printf("\n");    
     }
-
-    printf("%s", result);
-    free(result);
 }
 
 
-void multiplication_squared(size_t rows, size_t cols, int a[rows][cols], int b[rows][cols])
+void multiplication_squared(size_t rows, size_t cols, int** a, int** b)
 {
-    int result_matrix[rows][cols];
+    int** result_matrix = allocate_matrix(rows, cols);
 
     for (int i=0; i<rows; i++) 
     {
@@ -67,4 +68,5 @@ void multiplication_squared(size_t rows, size_t cols, int a[rows][cols], int b[r
     }
 
     print_matrix(rows, cols, result_matrix);
+    free_matrix(rows, cols, result_matrix);
 }
