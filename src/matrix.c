6483f0e29e69f7b5723c91ade9016064b1182155
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-29 15:56:42 +0100

    add measurements for execution

--- src/matrix.c
+++ src/matrix.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 {
     srand(time(0));
 
-    size_t sz = 5000;
+    size_t sz = 1000;
     int** a = generate_matrix(sz, sz);
     int** b = generate_matrix(sz, sz);
 
@@ -16,7 +16,13 @@ int main(int argc, char* argv[])
     print_matrix(sz, sz, b);
     printf("\n  =\n");
 
+    // measure exectution time
+    clock_t start = clock();
     int** result =  multiplication_squared(sz, sz, a, b);
+    clock_t end  = clock();
+
+    float diff = (float) (end - start) / CLOCKS_PER_SEC;
+    printf("exec. time: %f\n", diff);
 
     free_matrix(sz, sz, a);
     free_matrix(sz, sz, b);
