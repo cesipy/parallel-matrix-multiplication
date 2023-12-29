81fab443998a9dfbc6ef24ff50e32a20bc1092fb
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-29 08:06:59 +0100

    matrix multiplication for squared matrices

--- include/matrix.h
+++ include/matrix.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 void print_matrix(size_t rows, size_t cols, int (*matrix)[cols]);
+void multiplication_squared(size_t rows, size_t cols, int a[rows][cols], int b[rows][cols]);
 
 
 #endif
--- src/matrix.c
+++ src/matrix.c
@@ -2,6 +2,7 @@
 
 #define PADDING 4
 
+
 int main(int argc, char* argv[]) 
 {
     int matrix[4][4] = {{0, 1, 2, 3}, 
@@ -10,13 +11,15 @@ int main(int argc, char* argv[])
                         {12, 13, 14, 15}
                         };
 
-    int matrix1[3][3] = {{1, 0, 3}, {0, 0, 1}, {0, 1, 2}};
-    int matrix2[3][3] = {{3, 1, 1}, {1, 0, 1}, {1, 0, 1}};
+    int matrix1[3][3] = {{10, 1, 123}, {-5, 3, 11}, {7, -15, 9}};
+    int matrix2[3][3] = {{-14, 3, -2}, {6, 7, 8}, {-4, 1, 15}};
 
 
     print_matrix(3, 3, matrix2);
-    printf("\n-\n");
+    printf("\n*\n");
     print_matrix(3, 3, matrix1);
+    printf("\n=\n");
+    multiplication_squared(3, 3, matrix1, matrix2);
 
     return 0;
 }
@@ -43,4 +46,25 @@ void print_matrix(size_t rows, size_t cols, int matrix[rows][cols])
 
     printf("%s", result);
     free(result);
+}
+
+
+void multiplication_squared(size_t rows, size_t cols, int a[rows][cols], int b[rows][cols])
+{
+    int result_matrix[rows][cols];
+
+    for (int i=0; i<rows; i++) 
+    {
+        for (int j=0; j<cols; j++) 
+        {
+            int sum = 0;
+            for (int k=0; k<rows; k++)
+            {
+                sum += a[i][k] * b[k][j];
+            }
+            result_matrix[i][j] = sum;
+        }
+    }
+
+    print_matrix(rows, cols, result_matrix);
 }
